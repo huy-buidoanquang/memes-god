62610983850f8087fbdcd270b203d3d1b8dfaf1b
Pham Tien <pham.tien@ucg.co.nz>
2024-07-30 15:56:03 +0700

    add image - 1

---
+++ script.js
@@ -0,0 +1,17 @@
+const gallery = document.getElementById('gallery');
+const assetsFolder = './assets/';
+
+fetch(assetsFolder)
+    .then(response => response.text())
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const images = doc.querySelectorAll('a[href$=".jpg"], a[href$=".jpeg"], a[href$=".png"]');
+
+        images.forEach(image => {
+            const img = document.createElement('img');
+            img.src = assetsFolder + image.getAttribute('href');
+            gallery.appendChild(img);
+        });
+    })
+    .catch(error => console.error('Lỗi:', error));
